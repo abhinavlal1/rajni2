d9765fd8201b1507213d12a4f8233c493bfd86e6
abhinavlal1 <156417639+abhinavlal1@users.noreply.github.com>
2024-06-19 18:18:54 +0530

    nya vnet

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -2,3 +2,9 @@ module"rgs"{
     source="../../modules/azurerm-rg"
     rgs = var.rg_input
 }
+module"vnet"{
+    source = "../../modules/azurerm_vnet"
+    vnet_name = "vnet1"
+    rg_name="indiarg"
+    location="westeurope"
+}
